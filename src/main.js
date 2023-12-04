a8e9cfd951dcfff7a81003b3d69856da307b8806
Peter Jones <pjxcvi@proton.me>
2023-12-04 16:43:02 -0500

    add welcome()

--- src/main.js
+++ src/main.js
@@ -12,3 +12,20 @@ tellPromise
     console.log(fortune);
   })
   .catch(console.error);
+
+welcome()
+  .then(console.log) // Logs the result of welcome()
+  .then(tell) // Calls tell, which returns a rejected promise (no question supplied).
+  .then(console.log) // Skipped because tell returned a rejected promise.
+  .catch(console.error) // Logs error from tell() or welcome()
+  .then(goodbye) // Returns promise from goodbye()
+  .then(console.log) // Logs the result from goodbye()
+  .catch(console.error); // Logs error only from goodbye()
+
+// combine the results of two or more promises
+welcome()
+  .then((welcomeMessage) =>
+    goodbye().then((goodbyeMessage) => `${welcomeMessage}\n${goodbyeMessage}`)
+  ) // welcomeMessage and goodbyeMessage combined.
+  .then(console.log) // Logs combines messages
+  .catch(console.error); // Logs error from welcome() or goodbye()
