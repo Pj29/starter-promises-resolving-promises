67027e69ab3ccde60eb3888c0d20eee822891e79
Peter Jones <pjxcvi@proton.me>
2023-12-04 16:27:33 -0500

    add catch

--- src/main.js
+++ src/main.js
@@ -6,5 +6,9 @@ console.log(promise);
 
 const question = "Will the weather be nice today?";
 const tellPromise = tell(question);
-
-tellPromise.then(console.log);
+tellPromise
+  .then((fortune) => {
+    console.log(question);
+    console.log(fortune);
+  })
+  .catch(console.error);
